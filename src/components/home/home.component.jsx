6c6d36e73b547cc637aee50bfa8c2856f66a6050
yash Soni <tatva.dotnet.yashsoni@outlook.com>
2025-02-26 22:55:51 +0530

    some edit done

--- src/components/home/home.component.jsx
+++ src/components/home/home.component.jsx
@@ -26,52 +26,68 @@ const Home = () => {
       </Box>
 
 
-      <Box sx={{ display: 'flex',marginTop:'3rem',padding:'2rem' }}>
+      <Box sx={{ display: 'flex', marginTop: '3rem', padding: '2rem' }}>
 
         <Box sx={{ display: 'flex', flexDirection: 'column' }}>
 
 
-          <Typography variant='h6' sx={{color: '#13a8ff',width: '12%',background: '#d2edfd',padding: '0.3rem',borderRadius: '15px'}} >&#9679; About Us</Typography>
+          <Typography variant='h6' sx={{ color: '#13a8ff', width: '12%', background: '#d2edfd', padding: '0.3rem', borderRadius: '15px' }} >&#9679; About Us</Typography>
 
 
-          <Typography variant='h4' sx={{marginTop:'2rem',color:'#015789',fontFamily:'Cinzel'}}>Blueshield Energies</Typography>
+          <Typography variant='h4' sx={{ marginTop: '2rem', color: '#015789', fontFamily: 'Cinzel' }}>Blueshield Energies</Typography>
 
 
-          <Typography variant='h6' sx={{fontFamily:'Zain',width:'60%'}}>
+          <Typography variant='h6' sx={{ fontFamily: 'Zain', width: '60%' }}>
 
             Blueshield Energies is dedicated to making solar power simple, affordable, and accessible. We provide high-quality solar installations that help you cut electricity costs, reduce carbon footprints, and gain energy independence. With expert solutions tailored to your needs, we turn sunlight into savings—because the future is solar, and we’re here to power it.
 
           </Typography>
 
-             
-              <Typography variant='h6' sx={{display:'flex',alignItems:'center'}}>
-                      <StarIcon sx={{color:'#ebc600'}}/>  Excellant 4.8 / 5  |  <AvatarGroup total={54}>
-  <Avatar alt="Remy Sharp" src="/avatar1.jpg" />
-  <Avatar alt="Travis Howard" src="/avatar2.jpg" />
-  <Avatar alt="Agnes Walker" src="/avatar3.jpg" />
-  <Avatar alt="Trevor Henderson" src="/avatar4.jpg" />
-</AvatarGroup>
-              </Typography>
+
+          <Typography variant='h6' sx={{ display: 'flex', alignItems: 'center' }}>
+            <StarIcon sx={{ color: '#ebc600' }} />  Excellant 4.8 / 5  |  <AvatarGroup total={54}>
+              <Avatar alt="Remy Sharp" src="/avatar1.jpg" />
+              <Avatar alt="Travis Howard" src="/avatar2.jpg" />
+              <Avatar alt="Agnes Walker" src="/avatar3.jpg" />
+              <Avatar alt="Trevor Henderson" src="/avatar4.jpg" />
+            </AvatarGroup>
+          </Typography>
 
 
-              <Button sx={{color:'white',background:'#13a8ff',borderRadius:'8px',width:'15%',marginTop:'2rem',textTransform:'none',fontFamily:'Zain',letterSpacing:'1px',fontSize:'18px'}}> Learn More  &rarr;</Button>
+          <Button sx={{ color: 'white', background: '#13a8ff', borderRadius: '8px', width: '15%', marginTop: '2rem', textTransform: 'none', fontFamily: 'Zain', letterSpacing: '1px', fontSize: '18px' }}> Learn More  &rarr;</Button>
 
         </Box>
 
-        <Box sx={{display:'flex'}}>
+        <Box sx={{ display: 'flex' }}>
 
           <Box
             component="img"
             src="/AboutUs.jpg"
             alt="Description"
-            sx={{ width: "50%", height: "auto",borderRadius:'8px' }}
+            sx={{ width: "50%", height: "auto", borderRadius: '8px' }}
           />
 
           <Box>
 
-               <Box sx={{background:'transparent',backdropFilter:'blur(10px)'}}>
-                   <Typography>iuuigiug</Typography>
-               </Box>
+            <Box sx={{ borderRadius: '17px',background:'beige',
+    width: '20rem',
+    height: '20rem',
+    display: 'flex',
+    justifyContent: 'center',
+    marginLeft: '2rem',
+    alignItems: 'center' }}>
+              <Typography variant='h4' sx={{textAlign:'center'}}>50+ Customers satiesfied acrros PAN India</Typography>
+            </Box>
+
+            <Box sx={{ borderRadius: '17px',background:'beige',
+    width: '20rem',
+    height: '20rem',
+    display: 'flex',
+    justifyContent: 'center',
+    marginLeft: '2rem',
+    alignItems: 'center' }}>
+              <Typography variant='h4' sx={{textAlign:'center'}}>50+ Customers satiesfied acrros PAN India</Typography>
+            </Box>
           </Box>
 
         </Box>
